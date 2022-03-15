20303b9d440c1fa80fe73ddacbc7f8153e132bd1
Peter Hueer <private@peter-hueer.de>
2022-03-15 22:35:17 +0100

    Add YOutube Watermark

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,26 @@ import Crypto from "./Crypto/Crypto";
 import { useState } from "react";
 import Summary from "./Summary/Summary";
 import WaterMark from "./UI/WaterMark";
+import logo from "./assets/youtubelogo.svg";
 
 function App() {
   const [total, setTotal] = useState(0);
 
   return (
     <div className="app">
-      <WaterMark />
+      <div className="header">
+        <a
+          className="making-of"
+          target="_blank"
+          href="https://www.youtube.com/watch?v=x-cSqC5NRlE"
+        >
+          <img className="youtube" src={logo} />
+          <i>
+            Hier zum <b> Making Of </b> von der Webseite
+          </i>
+        </a>
+        <WaterMark />
+      </div>
       <Summary total={total} />
       <EtfGold onSum={(sum) => setTotal((prev) => prev + sum)} />
       <Crypto onSum={(sum) => setTotal((prev) => prev + sum)} />
