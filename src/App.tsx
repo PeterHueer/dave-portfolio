77b4e3298130a230183bc92134a7f294a5392d65
Peter Hueer <private@peter-hueer.de>
2022-04-01 16:48:54 +0200

    Remove all cookies

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,18 @@ import Summary from "./Summary/Summary";
 import WaterMark from "./UI/WaterMark";
 import logo from "./assets/youtubelogo.svg";
 
+
+function deleteCookies() {
+  var theCookies = document.cookie.split(';');
+  for (var i = 0 ; i < theCookies.length; i++) {
+      document.cookie = theCookies[i].split('=')[0] + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  }
+}
+
+
 function App() {
   const [total, setTotal] = useState(0);
+  deleteCookies();
 
   return (
     <div className="app">
